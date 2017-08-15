d9212a51f9aa68a1d1a18c59bcae234f9e8d13f2
Optimus Prime <Optimus Prime@BuzzLightyear>
2017-08-16 01:13:20 +0530

    Commit the userform

--- JioPhone/src/main/java/com/jio/booking/bo/UserForm.java
+++ JioPhone/src/main/java/com/jio/booking/bo/UserForm.java
@@ -1,18 +1,13 @@
 package com.jio.booking.bo;
 
 public class UserForm {
-	private int id;
+	
 	private String firstName;
 	private String lastname;
 	private String mobileNo;
 	private String email;
 	private String pincode;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -46,9 +41,7 @@ public class UserForm {
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("UserForm [id=");
-		builder.append(id);
-		builder.append(", ");
+		builder.append("UserForm [");
 		if (firstName != null) {
 			builder.append("firstName=");
 			builder.append(firstName);
@@ -76,58 +69,6 @@ public class UserForm {
 		builder.append("]");
 		return builder.toString();
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + id;
-		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
-		result = prime * result + ((mobileNo == null) ? 0 : mobileNo.hashCode());
-		result = prime * result + ((pincode == null) ? 0 : pincode.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		UserForm other = (UserForm) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (id != other.id)
-			return false;
-		if (lastname == null) {
-			if (other.lastname != null)
-				return false;
-		} else if (!lastname.equals(other.lastname))
-			return false;
-		if (mobileNo == null) {
-			if (other.mobileNo != null)
-				return false;
-		} else if (!mobileNo.equals(other.mobileNo))
-			return false;
-		if (pincode == null) {
-			if (other.pincode != null)
-				return false;
-		} else if (!pincode.equals(other.pincode))
-			return false;
-		return true;
-	}
-	
-	
 	
 
 }
