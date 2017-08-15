6df63f28b49e96789e622885756ac60d0d923d12
Optimus Prime <Optimus Prime@BuzzLightyear>
2017-08-16 00:40:12 +0530

    Commit the dao file

---
+++ JioPhone/src/main/java/com/jio/booking/service/JioBookingService.java
@@ -0,0 +1,7 @@
+package com.jio.booking.service;
+
+public interface JioBookingService {
+	
+	public void bookJioPhone();
+
+}
---
+++ JioPhone/src/main/java/com/jio/booking/service/JioBookingServiceImpl.java
@@ -0,0 +1,11 @@
+package com.jio.booking.service;
+
+public class JioBookingServiceImpl implements JioBookingService {
+
+	@Override
+	public void bookJioPhone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
