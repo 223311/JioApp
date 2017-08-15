33936b103e3cf1d069734f822205b95e08d668dd
Optimus Prime <Optimus Prime@BuzzLightyear>
2017-08-16 00:57:38 +0530

    commit

--- JioPhone/src/main/java/com/jio/booking/model/User.java
+++ JioPhone/src/main/java/com/jio/booking/model/User.java
@@ -2,15 +2,30 @@ package com.jio.booking.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER500")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(name = "FIRST_NAME", nullable = false, length = 50)
 	private String firstName;
+	@Column(name = "LAST_NAME", nullable = false, length = 50)
 	private String lastname;
+	@Column(name = "MOBILE_NO", nullable = false, length = 12)
 	private String mobileNo;
+	@Column(name = "EMAIL", nullable = false, length = 40)
 	private String email;
+	@Column(name = "FIRST_NAME", nullable = false, length = 7)
 	private String pincode;
 
 	public int getId() {
--- JioPhone/src/main/java/com/jio/booking/service/JioBookingServiceImpl.java
+++ JioPhone/src/main/java/com/jio/booking/service/JioBookingServiceImpl.java
@@ -1,6 +1,13 @@
 package com.jio.booking.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jio.booking.dao.UserDao;
+
 public class JioBookingServiceImpl implements JioBookingService {
+	
+	@Autowired
+	UserDao userdao;
 
 	@Override
 	public void bookJioPhone() {
