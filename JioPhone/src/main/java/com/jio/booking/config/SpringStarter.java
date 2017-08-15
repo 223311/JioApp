582000418e60e1578dd28ca1154de8f17532d37c
DIBYA <DIBYA@192.168.43.208>
2017-08-15 22:52:13 +0530

    Added The Spring Starter Class

--- JioPhone/src/main/java/com/jio/booking/config/SpringStarter.java
+++ JioPhone/src/main/java/com/jio/booking/config/SpringStarter.java
@@ -1,5 +1,25 @@
 package com.jio.booking.config;
 
-public class SpringStarter {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringStarter extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class<?>[] { RootConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] { "/" };
+	}
 
 }
