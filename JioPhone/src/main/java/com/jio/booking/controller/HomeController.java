4115dd3ffaf064412d0c935fb986f2100c467cad
Optimus Prime <Optimus Prime@BuzzLightyear>
2017-08-15 23:35:39 +0530

    commit the model

--- JioPhone/src/main/java/com/jio/booking/controller/HomeController.java
+++ JioPhone/src/main/java/com/jio/booking/controller/HomeController.java
@@ -5,10 +5,21 @@
  */
 package com.jio.booking.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author Optimus Prime
  */
+@Controller
+@RequestMapping("/home")
 public class HomeController {
-    
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
+
 }
